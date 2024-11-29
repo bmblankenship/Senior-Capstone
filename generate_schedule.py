f36cadabece96b2bdf31ce0108d60777256ae394
bmblankenship <brandonmblankenship1991@gmail.com>
2024-11-28 20:35:38 -0500

    Adding Python support for Schedule generation

---
+++ generate_schedule.py
@@ -0,0 +1,10 @@
+# Documentation for pylightxl library
+# https://pylightxl.readthedocs.io/en/latest/
+
+import pylightxl as xl
+
+# Get sheet location
+path = input("Enter file location: ")
+
+
+sheet = xl.readxl()
