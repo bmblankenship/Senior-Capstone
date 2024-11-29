88222bd2de539a75eee85819b861bd822bffc71f
bmblankenship <brandonmblankenship1991@gmail.com>
2024-11-28 20:44:04 -0500

    Getting setup to use python for scheduling

--- generate_schedule.py
+++ generate_schedule.py
@@ -3,8 +3,18 @@
 
 import pylightxl as xl
 
-# Get sheet location
-path = input("Enter file location: ")
+"""This file will take input of an excel sheet and generate a schedule for MATPOWER to run powerflow checks on."""
 
+# We will need to get the sheet location into here at some point
+# Likely will require some integration with the GUI
 
-sheet = xl.readxl()
+sheet = xl.readxl(fn='RequiredOutages.xlsx' , ws=('Transmission'))
+
+# Fetch branch number
+branches = sheet.ws(ws='Transmission').col(col=3)
+
+# Fetch outage duration (days)
+duration = sheet.ws(ws='Transmission').col(col=4)
+
+# Fetch other considerations
+considerations = sheet.ws(ws='Transmission').col(col=5)
