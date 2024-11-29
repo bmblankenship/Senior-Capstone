8c5cd932bfee0b344bf1d9efb91aaa344ca5fa5e
bmblankenship <brandonmblankenship1991@gmail.com>
2024-11-29 16:08:19 -0500

    Update generate_schedule.py
    Nothing major, just exploring the functionality of the library for excel sheet reading

--- generate_schedule.py
+++ generate_schedule.py
@@ -2,13 +2,11 @@
 # https://pylightxl.readthedocs.io/en/latest/
 
 import pylightxl as xl
-import re
 
 """This file will take input of an excel sheet and generate a schedule for MATPOWER to run powerflow checks on."""
 
 # We will need to get the sheet location into here at some point
 # Likely will require some integration with the GUI
-
 sheet = xl.readxl(fn='RequiredOutages.xlsx' , ws='Transmission')
 
 # Fetch branch number
@@ -31,16 +29,15 @@ print(duration)
 
 # Sanitize duration inputs
 
-i = 0
-for dura in duration:
-    if 'day' in dura or 'days' in dura:
+for i, dura in enumerate(duration):
+    if 'day' in dura in dura:
         dura = dura.replace('day', '')
         dura = dura.replace('s', '')
         dura = int(dura)
         dura = dura * 24
         duration[i] = dura
         i += 1
-    elif 'week' in dura or 'weeks' in dura:
+    elif 'week' in dura in dura:
         dura = dura.replace('week', '')
         dura = dura.replace('s', '')
         dura = int(dura)
@@ -48,4 +45,26 @@ for dura in duration:
         duration[i] = dura
         i += 1
 
+# Duration is now in hours
 print(duration)
+
+# Create output File
+# Below is a test case mostly to explore the functionality of the library.
+output = xl.Database()
+output.add_ws(ws="Schedule")
+
+output.ws(ws="Schedule").update_index(row = 1, col = 1, val = "Branch Number")
+for row_id, data in enumerate(branches, start = 2):
+    output.ws(ws="Schedule").update_index(row = row_id, col = 1, val = data)
+
+output.ws(ws="Schedule").update_index(row = 1, col = 2, val = "Duration (Hours)") 
+for row_id, data in enumerate(duration, start = 2):
+    output.ws(ws="Schedule").update_index(row = row_id, col = 2, val = data)
+
+output.ws(ws="Schedule").update_index(row = 1, col = 3, val = "Considerations") 
+for row_id, data in enumerate(considerations, start = 2):
+    if(data == ''):
+        data = ' '
+    output.ws(ws="Schedule").update_index(row = row_id, col = 3, val = data)
+
+xl.writexl(db = output, fn="schedule.xlsx")   
