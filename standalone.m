09cc76c84f15e1b38f2e454ce8b821e532bcd77d
bmblankenship <brandonmblankenship1991@gmail.com>
2024-12-08 15:04:28 -0500

    First version implementation of missing functions

--- standalone.m
+++ standalone.m
@@ -48,6 +48,36 @@ tic;
         4) If schedule fails, regenerate schedule and make sure it is not the same schedule
     %}
 
+    %
+    % NOT YET IMPLEMENTED
+    %
+    function [base_case] = base_schedule_case(schedule)
+        % Obtain the ranges that will be simulated from the schedule.
+        % This avoids running hours again after the original N-1 contingency that do not need to be reran
+        % Must be indexed to obtain the proper values later on.
+        % Format:
+        % Branch Number | Start Time | End Time
+
+        base_case_branch_outages = schedule(branch_number);
+        base_case_start_hours = schedule(start_time);
+        base_case_end_hours = schedule(end_time);
+
+        for i = 1:length(base_case_branch_outages) % Index through branches
+        end
+    end
+
+    %
+    % NOT YET IMPLEMENTED
+    %
+    function [block_dispatch] = gen_block_dispatch()
+    end
+
+    %
+    % NOT YET IMPLEMENTED
+    %
+    function error_handler()
+    end
+
     %generator outage
     function [generator_outage] = generator_outage(filename, tabname)
         generator_data = readtable(filename, "sheet", tabname);
