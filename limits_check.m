7e56a437387e8fb77a6d68c8ad83e97d8c4af3f9
bmblankenship <brandonmblankenship1991@gmail.com>
2025-01-23 09:28:22 -0500

    debugging work

--- limits_check.m
+++ limits_check.m
@@ -1,14 +1,20 @@
-function [limit_check_return, status] = limits_check(mpc_case)
+function [limit_check_return, failure_params] = limits_check(mpc_case)
     % limits_check - A function to verify health of the system under different outage cases
     %   Returns
     %       limit_check_return => Returns formatted strings to indicate either a full pass:
-    %       Pass: 11 PP
-    %       Otherwise indicates the failure mode and branches or busses the failure was located at.
-    %       Failure: 00 106||109
+        %       Pass: 11 PP
+        %       Otherwise indicates the failure mode and branches or busses the failure was located at.
+        %       Failure: 00 106||109
+    %       status => returns true or false for the limit checks being violated to trigger datacollection for examination
     %   Inputs
     %       mpc_case is the result of a powerflow from runpf.
+    failure_params = struct;
+    failure_params().vmag = [];
+    failure_params().MVA = [];
+    failure_params.status = true;
+    i = 1;
+
     MVA_success_flag = true;
-    MVA_failure_branch = 'P';
     for n = 1:height(mpc_case.branch)
         s1 = sqrt(mpc_case.branch(n,14)^2 + mpc_case.branch(n,15)^2);
         s2 = sqrt(mpc_case.branch(n,16)^2 + mpc_case.branch(n,17)^2);
@@ -18,38 +24,34 @@ function [limit_check_return, status] = limits_check(mpc_case)
             apparent_power = s2;
         end
 
-        if((apparent_power > mpc_case.branch(n,6)) && MVA_success_flag)
-            MVA_failure_branch = int2str(n);
+        if((apparent_power > mpc_case.branch(n,6)))
+            failure_params(i).MVA = n;
+            failure_params.status = false;
             MVA_success_flag = false;
-        elseif(apparent_power > mpc_case.branch(n,6))
-            MVA_failure_branch = append('/', int2str(n));
+            i = i + 1;
         end
     end
+    i = 0;
 
     voltage_success_flag = true;
-    voltage_failure_bus = 'P';
     for n = 1:height(mpc_case.bus)
-        if((mpc_case.bus(n,8) >= 1.1 || mpc_case.bus(n,8) <= 0.9) && voltage_success_flag)
-            voltage_failure_bus = int2str(n);
+        if((mpc_case.bus(n,8) >= 1.1 || mpc_case.bus(n,8) <= 0.9))
+            failure_params(i).vmag = n;
+            failure_params.status = false;
             voltage_success_flag = false;
-        elseif(mpc_case.bus(n,8) >= 1.1 || mpc_case.bus(n,8) <= 0.9)
-            voltage_failure_bus = append('/', int2str(n));
+            i = i + 1;
         end
     end
     
     % Return format
     % VMag MVAMag Bus Branch
     if(~voltage_success_flag && ~MVA_success_flag)
-        limit_check_return = append('00 ', voltage_failure_bus, '||', MVA_failure_branch);
-        status = false;
+        limit_check_return = '00';
     elseif(voltage_success_flag && ~MVA_success_flag)
-        limit_check_return = append('10 ', voltage_failure_bus, '||', MVA_failure_branch);
-        status = false;
+        limit_check_return = '11';
     elseif(~voltage_success_flag && MVA_success_flag)
-        limit_check_return = append('01 ', voltage_failure_bus, '||', MVA_failure_branch);
-        status = false;
+        limit_check_return = '01';
     else
-        limit_check_return = append('11 ', voltage_failure_bus, '||', MVA_failure_branch);
-        status = true;
+        limit_check_return = '11';
     end
 end
--- n1_contingency.m
+++ n1_contingency.m
@@ -1,8 +1,8 @@
-function [results_array, mpc_array, failure_array] = n1_contingency(settings, lineout, generation_outages, load_data, mpc, gen_array, block_dispatch, mpopt, start_hour, end_hour)
+function [results_array, failure_array] = n1_contingency(settings, lineout, generation_outages, load_data, mpc, gen_array, block_dispatch, mpopt, start_hour, end_hour)
     % n1_contingency - A function to simulate n-1 contingency for power systems.
     %   Returns
     %       results_array => Returns the results of the limits_check function, detailing the health of the system in terms of MVA and Voltage Magnitude limits
-    %       mpc_array   => Returns the systems the powerflow was run on each for each contingency hour
+    %       failure_array => Returns hours, branch outage and powerflow results from any cases that failed the limit checks
     %   Inputs
     %       settings is the settings class object used to pass settings parameters.
     %       LINEOUT NEEDS TO BE CHANGED TO TRANSMISSION OUTAGE OBJECT FOR THE CASES THAT MULTIPLE LINES ARE OUT.
@@ -14,7 +14,7 @@ function [results_array, mpc_array, failure_array] = n1_contingency(settings, li
     %       mpopt is the options for the powerflow including algorithm type.
     %       start_hour is the start hour of the simulation. Settings.start_hour is used for the initial case and is set in settings.txt
     %       end_hour is the end hour of the simulation. Settings.end_hour is used for the initial case and is set in settings.txt
-    
+    block_disp = settings.block_dispatch;
     for k = start_hour:end_hour
         if(mod(k,10) == 0)
             disp("N-1 Contingency Hour: " + k);
@@ -56,7 +56,7 @@ function [results_array, mpc_array, failure_array] = n1_contingency(settings, li
             temp_isl_mpc = extract_islands(partempmpc, 1);
 
             %block dispatch
-            if(settings.block_dispatch == true)
+            if(block_disp == true)
                 temp_isl_mpc = gen_scale(temp_isl_mpc, block_dispatch, k, gen_array(1), gen_array(2), gen_array(3), gen_array(4), gen_array(5));
             else
                 temp_isl_mpc.gen(:,2) = temp_isl_mpc.gen(:,2) * par_temp_load_data.weighted_load(k);
@@ -68,16 +68,24 @@ function [results_array, mpc_array, failure_array] = n1_contingency(settings, li
             temp_isl_mpc.bus(:,4) = temp_isl_mpc.bus(:,4) * par_temp_load_data.weighted_load(k); %Reactive power scaling
 
             results = runpf_wcu(temp_isl_mpc, mpopt);
-            [limit_results, success] = limits_check(results);
+            [limit_results, failure_params] = limits_check(results);
             results_array{k,n} = limit_results;
 
-            if(~success)
-                mpc_array{k,n} = results;
-                failure_array{k,n} = failure_info(k, n);
+            if(~failure_params.status)
+                failure = struct;
+                failure.hour = k;
+                failure.branch_out = n;
+                failure.branch = results.branch;
+                failure.bus = results.bus;
+                failure.gen = results.gen;
+                failure.vmag = failure_params.vmag;
+                failure.mva = failure_params.MVA;
+                failure_array{k,n} = failure;
             end
             
             partempmpc.branch(n,11) = 1;
 
         end %for loop
     end %parfor loop
+    failure_array = failure_array(~cellfun('isempty', failure_array));
 end
