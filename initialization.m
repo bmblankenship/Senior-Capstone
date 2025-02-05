94f1a2bd1cf4e6f1627f2d0cccab6e2c81b410d7
bmblankenship <brandonmblankenship1991@gmail.com>
2025-02-05 12:20:42 -0500

    changed testing params

--- initialization.m
+++ initialization.m
@@ -45,7 +45,7 @@ function initialization()
     
     %initial N-1 contingency to verify health of the system with planned generator outages
     disp("Starting N-1 Contingency Analysis");
-    initial_n1_outage =  scheduled_outage(false, 0, 0, []);
+    initial_n1_outage =  scheduled_outage(true, 1, 2200, [17]);
     [ini_results, ini_failure] = n1_contingency(sim_settings, initial_n1_outage, generation_outages, load_data_obj, mpc, gen_array, block_dispatch, mpopt, sim_settings.start_hour, sim_settings.simulation_hours);
     assignin('base', 'initial_results_array', ini_results);
     assignin('base', 'initial_failure_array', ini_failure);
