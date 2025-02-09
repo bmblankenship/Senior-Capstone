d8867a85f9c35bbc63b12a2adf6fb01410d9baee
bmblankenship <brandonmblankenship1991@gmail.com>
2025-02-09 10:26:19 -0500

    Delete base_schedule_case.m

--- base_schedule_case.m
+++
@@ -1,8 +0,0 @@
-%% Base Case NYI
-function [base_case] = base_schedule_case(schedule)
-    base_case = [height(schedule),2];
-
-    for i = 1:height(schedule) % Index through branches
-        base_case(i,2) = schedule(i);
-    end
-end
